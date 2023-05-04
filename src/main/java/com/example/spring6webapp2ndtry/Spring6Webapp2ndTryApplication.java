e26112d1af99ee176c500dd691bc96ed7b1670f2
comp_theme <iraklitsulukidze14@gmail.com>
2023-05-04 22:32:36 +0400

    initialization

---
+++ src/main/java/com/example/spring6webapp2ndtry/Spring6Webapp2ndTryApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring6webapp2ndtry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring6Webapp2ndTryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring6Webapp2ndTryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/spring6webapp2ndtry/Spring6Webapp2ndTryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring6webapp2ndtry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring6Webapp2ndTryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
